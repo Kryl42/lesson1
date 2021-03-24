f140fb4addd1d1a7441a7178c4ab488b5a8acafd
Константин <Kryl42@mail.ru>
2021-03-24 12:12:00 +0300

    первая версия

---
+++ funkc.py
@@ -0,0 +1,21 @@
+def dicounted(price,discount,max_discount=50):
+# max_discount именованный элемент, указывать его необязательно,
+# если не указать то  то приймет заранее заданнное значение
+    if type(price)!= int:
+        raise ValueError('Укажите цену цифрами')
+    price = abs(float(price))
+    discount = abs(float(discount))
+    max_discount = abs(float(max_discount))
+    
+    if max_discount >90:
+        raise ValueError('Максимальная скидка не может быть больше 90%')
+    # обработка исключений если выполняется условие то выводится ошибка
+    # сообщение  указанное в raise
+    if discount > max_discount:
+        price_with_discount = price
+    else:
+        price_with_discount = price - price * discount/100
+
+    return price_with_discount
+
+print(dicounted("сто",60,75))
---
+++ lesson1.2.py
@@ -0,0 +1,22 @@
+# урать пробелы из строки(только в начале и в конце)
+a='     котя  '
+b = a.strip()
+print(b)
+#отсутствие значений None
+a = None
+b=0
+print(a is None)
+print(b is  None)
+print(b is not None)
+# Типы данных
+a = 2
+b = 3.0
+c= '5'
+d=True
+s=None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(s))
+
