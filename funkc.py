2e99b158d0833505c9a3259099cdea57d6cbaaab
Kryl42 <Kryl42@mail.ru>
2021-04-17 09:37:10 +0300

    снова учусь

--- funkc.py
+++ funkc.py
@@ -1,13 +1,13 @@
-def dicounted(price,discount,max_discount=50):
-# max_discount именованный элемент, указывать его необязательно,
-# если не указать то  то приймет заранее заданнное значение
-    if type(price)!= int:
+def dicounted(price, discount, max_discount=50):
+    # max_discount именованный элемент, указывать его необязательно,
+    # если не указать то  то приймет заранее заданнное значение
+    if type(price) != int:
         raise ValueError('Укажите цену цифрами')
     price = abs(float(price))
     discount = abs(float(discount))
     max_discount = abs(float(max_discount))
-    
-    if max_discount >90:
+
+    if max_discount > 90:
         raise ValueError('Максимальная скидка не может быть больше 90%')
     # обработка исключений если выполняется условие то выводится ошибка
     # сообщение  указанное в raise
@@ -18,4 +18,5 @@ def dicounted(price,discount,max_discount=50):
 
     return price_with_discount
 
-print(dicounted("сто",60,75))
+
+print(dicounted(500, 60, 75))
